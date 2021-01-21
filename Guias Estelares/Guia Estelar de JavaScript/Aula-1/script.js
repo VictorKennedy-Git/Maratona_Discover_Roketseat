f350e8043fa682b5d5273f6a5620c8059875d50f
Kennedy <67836881+VictorKennedy-Git@users.noreply.github.com>
2021-01-21 11:13:29 -0300

    Revisando Estudo de Java
    Inicianando com estudos básico da linguangem... console.log(), tipos de dados... dois primeiros capítulos do guia estelar de js

---
+++ Guias Estelares/Guia Estelar de JavaScript/Aula-1/script.js
@@ -0,0 +1,23 @@
+//Aqui tem um comentário em linha
+
+/*
+AQUI UM COMENTÁRIO DE BLOCO
+*/
+
+// var oi = "EU TO DOIDO";
+// console.log(`olá mundo`)
+
+
+// console.log( {
+// 	name: "maike",
+// 	idade: 18,
+// 	andar: function () {
+// 		console.log(`Andar`)
+// 	}
+// })
+
+console.log([
+	"leite",
+	"ovos",
+	"bolo"
+])
